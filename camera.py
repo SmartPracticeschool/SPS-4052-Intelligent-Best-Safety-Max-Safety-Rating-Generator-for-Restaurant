12ccf37c77321d193400f6787b16f1ef9e7a155a
nabinbasak <nabinbasaknb@gmail.com>
2020-09-14 16:09:58 +0530

    first commit

---
+++ camera.py
@@ -0,0 +1,55 @@
+import cv2
+import boto3
+import datetime
+import requests
+face_cascade=cv2.CascadeClassifier("haarcascade_frontalface_alt2.xml")
+ds_factor=0.6
+
+count=0
+
+class VideoCamera(object):    
+    def __init__(self):
+        self.video = cv2.VideoCapture(0)
+    
+    def __del__(self):
+        self.video.release()
+    
+    def get_frame(self):
+        #count=0
+        global count
+        success, image = self.video.read()
+        is_success, im_buf_arr = cv2.imencode(".jpg", image)
+        image1 = im_buf_arr.tobytes()
+
+        client = boto3.client('rekognition',
+                              aws_access_key_id="AKIAURCTNX7BN76X7ROA",
+                              aws_secret_access_key="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",
+                              region_name='us-east-2')
+
+        response = client.detect_custom_labels(
+        ProjectVersionArn='arn:aws:rekognition:us-east-2:311560093634:project/Mask-Detection/version/Mask-Detection.2020-09-11T21.05.12/1599838550732',Image={
+            'Bytes':image1})
+        print(response['CustomLabels'])
+        
+        if not len(response['CustomLabels']):
+            count=count+1
+            date = str(datetime.datetime.now()).split(" ")[0]
+            #print(date)
+            url = "https://fn4dq77658.execute-api.us-east-2.amazonaws.com/apiForMaskCount?date="+date+"&count="+str(count)
+            resp = requests.get(url)
+            f = open("countfile.txt", "w")
+            f.write(str(count))
+            f.close()
+            #print(count)
+
+        image=cv2.resize(image,None,fx=ds_factor,fy=ds_factor,interpolation=cv2.INTER_AREA)
+        
+        gray=cv2.cvtColor(image,cv2.COLOR_BGR2GRAY)
+        face_rects=face_cascade.detectMultiScale(gray,1.3,5)
+        for (x,y,w,h) in face_rects:
+        	cv2.rectangle(image,(x,y),(x+w,y+h),(0,255,0),2)
+        	break
+        ret, jpeg = cv2.imencode('.jpg', image)
+        #cv2.putText(image, text = str(count), org=(10,40), fontFace=cv2.FONT_HERSHEY_PLAIN, fontScale=1, color=(1,0,0))
+        cv2.imshow('image',image)
+        return jpeg.tobytes()
